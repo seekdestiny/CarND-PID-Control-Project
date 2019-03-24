53bc8785a49f44edac3d21fa9f98f9a049344cae
seekdestiny <jifeiqian@gmail.com>
2019-03-23 17:39:28 -0700

    complete path planning code

--- src/PID.h
+++ src/PID.h
@@ -4,6 +4,20 @@
 class PID {
  public:
   /**
+   * PID Errors
+   */
+  double p_error;
+  double i_error;
+  double d_error;
+
+  /**
+   * PID Coefficients
+   */ 
+  double Kp;
+  double Ki;
+  double Kd;
+
+  /**
    * Constructor
    */
   PID();
@@ -17,8 +31,9 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
-
+  void Init(double Kp, double Ki, double Kd, unsigned int calc_tot_err_after, unsigned int tune_coeffs_each,
+            double twiddle_dKp_initial, double twiddle_dKi_initial, double twiddle_dKd_initial,
+            double twiddle_tolerance);
   /**
    * Update the PID error variables given cross track error.
    * @param cte The current cross track error
@@ -32,19 +47,32 @@ class PID {
   double TotalError();
 
  private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+  /*
+  * Helper variables
+  */
+  unsigned int _calc_tot_err_after;
+  unsigned int _tune_coeffs_each;
+  unsigned long long _step_cnt;
 
-  /**
-   * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+  /*
+  * Helper variables for TWIDDLE algorithm
+  */
+  double _dKp;
+  double _dKi;
+  double _dKd;
+  double _tol;
+  double _best_err;
+  double _total_err;
+  int _coeff_ind;
+  int _attempts_per_ind;
+
+  /*
+  * Tune parameters in accordance with the "TWIDDLE" algorithm for parameter tuning.
+  */
+  void TuneCoeffsUsingTwiddleAlg();
+
+  double &IndToCoeff(int ind);
+  double &IndToDeltaCoeff(int ind);
 };
 
 #endif  // PID_H
--- src/main.cpp
+++ src/main.cpp
@@ -33,12 +33,26 @@ string hasData(string s) {
 int main() {
   uWS::Hub h;
 
-  PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
+  unsigned long long ob_cnt = 0; // number of elapsed timestamps
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  // steering and throttle PID controllers
+  PID pid_steer, pid_throttle;
+
+  // How the parameters were tuned (manually + twiddle):
+  // steering: no twiddle -- P=0.2, I=0.004, D=3.0; throttle: 0.3 fixed
+  // steering: twiddle -- P=0.2, I=0.004, D=3.0; throttle: 0.3 fixed
+  // steering: twiddle -- P=0.35, I=0.0188, D=10.0; throttle: 0.3 fixed
+  // steering: no twiddle -- P=0.64, I=0.0238, D=14.0; throttle: twiddle -- P=2.0, I=0.0, D=3.0
+  // steering: no twiddle -- P=0.64, I=0.0238, D=14.0; throttle: twiddle -- P=2.5, I=0.0, D=8.0
+  // steering: twiddle -- P=0.35, I=0.0188, D=10.0; throttle: twiddle -- P=4, I=0.0, D=9.0
+  // steering: no twiddle -- P=0.2, I=0.0188, D=7.0; throttle: no twiddle P=2.5, I=0.0, D=8.0
+  // steering: no twiddle -- P=0.15, I=0.0188, D=5.0; throttle: no twiddle P=4.5, I=0.0, D=8.0
+  // steering: no twiddle -- P=0.15, I=0.0188, D=5.0; throttle: no twiddle P=6.0, I=0.0, D=2.0
+
+  pid_steer.Init(0.15, 0.0188, 5.0, 30, 0, 0.1, 0.005, 1.0, 0.2);
+  pid_throttle.Init(6.0, 0.0, 2.0, 30, 0, 0.5, 0.005, 1.0, 0.1);
+  
+  h.onMessage([&pid_steer, &pid_throttle, &ob_cnt](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -56,21 +70,50 @@ int main() {
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
-          double steer_value;
+          double steer_value, throttle_value;
           /**
            * TODO: Calculate steering value here, remember the steering value is
            *   [-1, 1].
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+         
+          // update error and calculate steer_value at each step
+          pid_steer.UpdateError(cte);
+          steer_value = pid_steer.TotalError();
 
+          pid_throttle.UpdateError(std::max(fabs(steer_value), fabs(cte)));
+          throttle_value = 1.0 + pid_throttle.TotalError(); 
+          if (throttle_value > 1.0) {
+            throttle_value = 1.0;
+          } else if (throttle_value <= 0.0) {
+            throttle_value = 0.3;
+          }
+
+          // increment timestamp
+          ++ob_cnt;
+
+          // DEBUG
+          std::cout
+              << "\n======[" << ob_cnt << "]======"
+              << "\nCross Track Error:               " << cte
+              << "\nCurrent Speed:                   " << speed
+              << "\nCurrent Steering Angle:          " << angle
+              << "\nPID Corrected Steering:          " << steer_value
+              << "\nPID Steering Coefficients:       " <<   "Kp = " << pid_steer.Kp
+                                                       << ", Ki = " << pid_steer.Ki
+                                                       << ", Kd = " << pid_steer.Kd
+              << "\nPID Corrected Throttle:          " << throttle_value
+              << "\nPID Throttle Coefficients:       " <<   "Kp = " << pid_throttle.Kp
+                                                       << ", Ki = " << pid_throttle.Ki
+                                                       << ", Kd = " << pid_throttle.Kd
+              << "\n======[" << ob_cnt << "]======\n"
+              << std::endl;
+
+          // operate on vehicle
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle_value;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
